ccf58c7bb20654cc5abed0a846dd676996dc2309
Muslim Tech _ مسلم تيك <moslimtech41@gmail.com>
2025-09-24 18:46:03 +0300

    Update dashboard.js

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -20,10 +20,12 @@ const AUTO_REFRESH_INTERVAL = 600000; // 30 ثانية
 
 
 // // أضف Service Worker registration هنا
+/* ==== تسجيل Service Worker ==== */
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
-      .then(reg => console.log('Service Worker registered'))
+    navigator.serviceWorker
+      .register('../service-worker.js')  // من مجلد dashboard إلى الجذر
+      .then(reg => console.log('Service Worker registered', reg))
       .catch(err => console.warn('SW registration failed:', err));
   });
 
